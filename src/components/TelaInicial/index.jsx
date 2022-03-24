ea52895f462d3dd8bd5736772f7d99128d9c4623
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-03-24 11:39:52 -0300

    Get Filmes concluído

--- src/components/TelaInicial/index.jsx
+++ src/components/TelaInicial/index.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import { Main, DivCima, SelecioneH1, DivImagem, Cartaz } from "./style.js";
 
 import Filme from "./../../assets/images/batman.jpg";
 
 function TelaInicial() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const requisicao = axios.get(
+      "https://mock-api.driven.com.br/api/v5/cineflex/movies"
+    );
+
+    requisicao.then((resposta) => {
+      const { data } = resposta;
+      setFilmes(data);
+    });
+    requisicao.catch((err) => console.log(err.resposta));
+  }, []);
   return (
     <>
       <DivCima>
@@ -15,24 +31,14 @@ function TelaInicial() {
         <DivImagem>
           <Cartaz src={Filme} alt="Filme em Cartaz" />
         </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
-        <DivImagem>
-          <Cartaz src={Filme} alt="Filme em Cartaz" />
-        </DivImagem>
+        {filmes.map((filme) => {
+          const { id, title, posterURL } = filme;
+          return (
+            <DivImagem key={id}>
+              <Cartaz src={posterURL} alt={title} />
+            </DivImagem>
+          );
+        })}
       </Main>
     </>
   );
